f0829b035020f084e27527d7f8287b887b10ced6
laumcs21 <lauram.cardenass@uqvirtual.edu.co>
2025-11-12 23:37:12 -0500

    Fix: eliminar Components (mayúscula) definitivamente

--- inicio/src/Components/ad/organisms/top-bar/top-bar.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'ad-top-bar',
-  templateUrl: './top-bar.component.html',
-  styleUrls: ['./top-bar.component.css'],
-  standalone: false
-})
-export class TopBarComponent {}
